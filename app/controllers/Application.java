a4e66ae962f7df20f3ff150dae1cbb3953c6540d
Nicolai Löffler <nicolai.loeffler@htwg-konstanz.de>
2014-10-22 15:16:07 +0200

    buttons & text field

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,18 +12,18 @@ public class Application extends Controller {
 	static IFiveWinsController controller;
 	
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework",8));
+        return ok(views.html.index.render());
     }
     
     public static Result newGame(Integer fieldSize) {
     	controller = new FiveWinsController(new Field(new Integer(fieldSize)));
     	String fieldString = controller.getFieldString();
-    	return ok(views.html.index.render(fieldString, fieldSize));
+    	return ok(views.html.field.render(fieldString, fieldSize));
     }
 
     public static Result setCell(String column, String row){
     	controller.handleInputOrQuit(column+","+row);
-    	return ok(views.html.index.render(controller.getFieldString(),8));
+    	return ok(views.html.field.render(controller.getFieldString(),8));
     }
     
     
